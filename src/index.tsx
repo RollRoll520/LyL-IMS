7fa51b2a7c16f1b1765b4baefbff5fb91632d8cd
liu <u202015604@hust.edu.cn>
2023-06-09 16:51:12 +0800

    establish

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; //配置
+import { ConfigProvider } from 'antd';
+import zhCN from "antd/lib/locale/zh_CN";
+import TestReactSpring from './pages/testReactSpring/testReactSpring';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Router>
+    <ConfigProvider locale={zhCN}>
+      <Routes>
+        <Route path="/*" element={<TestReactSpring />} />
+        <Route path="/lyl/*" element={<App />} />
+      </Routes>
+    </ConfigProvider>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
